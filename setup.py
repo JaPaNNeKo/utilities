aa6ea5af32ab24aa9abb55ac2514e3011a0687bb
JaPaNNeKo <maximefer@hotmail.fr>
2021-08-07 21:00:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(
-  name = 'package',         # How you named your package folder (MyLib)
+  name = 'utilities_perso',
   packages = ['yggdrasil'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
