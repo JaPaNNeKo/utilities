c46f5d24a03576b8bf60eb007f619ecaa1c3660b
JaPaNNeKo <maximefer@hotmail.fr>
2021-08-22 00:26:27 +0100

    Remaking setup files

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'utilities_perso',
-  packages = ['yggdrasil'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  name='yggdrasil',
+  packages=['yggdrasil'],   # Chose the same as "name"
+  version='1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Test package to upload to github',   # Give a short description about your library
-  author = 'NeKo',                   # Type in your name
-  url = 'https://github.com/JaPaNNeKo/utilities',   # Provide either the link to your github or to your website
-  keywords = ['utilities', 'yggdrasil',],   # Keywords that define your package best
+  description='Apps handler for in-house python scripts',   # Give a short description about your library
+  author='NeKo',                   # Type in your name
+  url='https://github.com/JaPaNNeKo/yggdrasil',   # Provide either the link to your github or to your website
+  keywords=['utilities', 'yggdrasil',],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'virtualenvwrapper-win',
       ],
+  include_package_data=True,
 )
