abc844be453d3f5d5b8eaeccdff1f867b979d7d4
mx-personal <maximefer@hotmail.fr>
2022-04-03 01:02:11 +0100

    debugging app_web
    Work in progress on app_web --> First working draft of create method, tested with tool_sample repo

--- yggdrasil/app/app_web.py
+++ yggdrasil/app/app_web.py
@@ -4,6 +4,7 @@ from yggdrasil.logger import logger
 import os
 import yaml
 import yggdrasil.informer.main as informer
+from ygg_helpers.main import DistInfo
 
 class AppWeb(AppGeneric):
     _identifier = 'web'
@@ -32,48 +33,45 @@ class AppWeb(AppGeneric):
                 cmds.append(r'py -m venv {0}'.format(path_venv))
             else:
                 cmds.append(r'py -{0} -m venv {1}'.format(self.version_py, path_venv))
-            cmds.append(r'{0}\Scripts\activate && pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org {1} && deactivate'.format(path_venv, self.url))
+            cmds.append(r'{0}\Scripts\activate && pip install --no-cache-dir --trusted-host pypi.org --trusted-host files.pythonhosted.org {1} && deactivate'.format(path_venv, self.url))
             # TODO parametrise ygg-helpers url
             # TODO remove @improvements
-            cmds.append(r'{0}\Scripts\activate && pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org {1} && deactivate'
-                        .format(path_venv, 'git+https://github.com/mx-personal/yggdrasil.git@improvements'))
+            cmds.append(r'{0}\Scripts\activate && pip install --no-cache-dir --trusted-host pypi.org --trusted-host files.pythonhosted.org {1} && deactivate'
+                        .format(path_venv, 'git+https://github.com/mx-personal/ygg_helpers.git@corrections'))
 
-            # TODO README req for github tools - no dash, no space on repo name (or make ygg replace - with _ for dist info finding?)
+            # TODO README req for github tools - no dash, no space on repo name (or make ygg replace - with _ for dist info finding?) - Not sure that's actually a problem, to test out
             cmds.append(r"{0}\Scripts\activate && gen_dist_info {1} && deactivate".format(path_venv, self.repo_name))
-            cmds.append(r"{0}\Scripts\activate && gen_dist_info {1} && deactivate".format(path_venv, "yggdrasil"))
+            cmds.append(r"{0}\Scripts\activate && gen_dist_info {1} && deactivate".format(path_venv, "ygg_helpers"))
 
-            print(cmds)
-            import pdb;pdb.set_trace()
             run_cmds(cmds)
             cmds = []
-            import pdb;pdb.set_trace()
             run_cmds(cmds)
 
             # TODO will leave some trash, clean up dependencies too
             # TODO keep if debug mode, delete otherwise
-            info_repo = informer.DistInfo.from_yaml(r'{0}\ygginfo-{1}.yaml'.format(path_venv, self.repo_name))
-            info_ygg_help = informer.DistInfo.from_yaml(r'{0}\ygginfo-yggdrasil.yaml'.format(path_venv))
+            info_repo = DistInfo.from_yaml(r'{0}\ygginfo-{1}.yaml'.format(path_venv, self.repo_name))
+            info_ygg_help = DistInfo.from_yaml(r'{0}\ygginfo-ygg_helpers.yaml'.format(path_venv))
 
-            cmds = [r"{0}\Scripts\activate && pip uninstall yggdrasil"]
+            cmds = [r"{0}\Scripts\activate && pip uninstall -y ygg_helpers".format(path_venv)]
             run_cmds(cmds)
 
             # TODO Parametrise bypassing SSL security
             cmds = [r"{0}\Scripts\activate && pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org  -r {1}".format(path_venv, req.path) for req in info_repo.requirements]
+            # import pdb;pdb.set_trace()
             run_cmds(cmds)
 
         # TODO Reinclude debugging & cmd error management differently
 
         # Generate batch launcher
         map_replac_eps = [[
-            ("#path_venvs#", path_venv),
-            ("#name_venv#", self.venv_name),
+            ("#path_venv#", path_venv),
             ("#entry_point#", ep.path),
         ] for ep in info_repo.entry_points]
 
-        for map in map_replac_eps:
+        for k, map in enumerate(map_replac_eps):
             generate_custom_batch(
                 source=r'{0}\template_launcher_web.txt'.format(path_templates),
-                destination=r'{0}\{1}.bat'.format(path_scripts, self.name),
+                destination=r'{0}\{1}.bat'.format(path_scripts, info_repo.entry_points[k].name),
                 replacements=map,
             )
 
